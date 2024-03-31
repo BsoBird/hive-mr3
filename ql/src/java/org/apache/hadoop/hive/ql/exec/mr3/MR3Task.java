baf2c04c3bcd88d10f68cb768f48dbe9d44d230e
Sungwoo Park <glapark@datamonad.com>
2024-03-31 00:04:54 -0700

    Minor: Fix MR3Task, ObjectCacheFactory, MRCompactor for compilation

--- ql/src/java/org/apache/hadoop/hive/ql/exec/mr3/MR3Task.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/mr3/MR3Task.java
@@ -73,6 +73,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
